d035da05ddec98ba08f94f0ac104aba01c9424cf
Doug <dougjchau@gmail.com>
2014-03-17 19:22:06 -0400

    add tweek.urls

--- twitch/urls.py
+++ twitch/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'twitch.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'', include('tweek.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
