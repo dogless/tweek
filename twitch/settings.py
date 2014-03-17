8603fd2dc244197040c8762f1912b70a5973cc7f
Doug <dougjchau@gmail.com>
2014-03-17 19:08:01 -0400

    fix local postgres

--- twitch/settings.py
+++ twitch/settings.py
@@ -57,8 +57,7 @@ WSGI_APPLICATION = 'twitch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.',
     }
 }
 
@@ -83,7 +82,7 @@ STATIC_URL = '/static/'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(default='postgres://a:a@localhost/twitchdb')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
