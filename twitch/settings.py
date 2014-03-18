6acf16f92f1b40e83c0b59f82fa6b1f6b4a68614
Doug <dougjchau@gmail.com>
2014-03-17 21:14:38 -0400

    add bootstrap

--- twitch/settings.py
+++ twitch/settings.py
@@ -103,11 +103,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 )
