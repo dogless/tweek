c25a5f2ae3cf5a990b0e5bf489fb567f4d6c6af4
Doug <dougjchau@gmail.com>
2014-03-18 15:43:44 -0400

    implement working next button (still need cleaning)

--- tweek/views.py
+++ tweek/views.py
@@ -6,15 +6,7 @@ import json
 
 # Create your views here.
 def index(request):
-	base = "https://api.twitch.tv/kraken"	
-	req = urllib2.Request(base + "/videos/top")
-	req.add_header('Accept', 'application/vnd.twitchtv.v2+json')
-	opener = urllib2.build_opener()
-	f = opener.open(req)
-	jsonObject = json.load(f)
-	video = jsonObject.get("videos")[0]
-	soup = BeautifulSoup(video.get('embed', None))
-	embedData = soup.find('param', attrs = {'name' : 'flashvars'})['value']
+	embedData = grabVideo(0)
 	context = {
 			'video' : embedData
 	}
@@ -22,5 +14,20 @@ def index(request):
 
 def next(request):
 	results = {'success':'False'}
+	embedData = grabVideo(1)
+	results['embed'] = embedData
+	results['success'] = 'True'
 	jason = json.dumps(results)
 	return HttpResponse(jason, content_type='application/json')
+
+def grabVideo(number):
+	base = "https://api.twitch.tv/kraken"	
+	req = urllib2.Request(base + "/videos/top")
+	req.add_header('Accept', 'application/vnd.twitchtv.v2+json')
+	opener = urllib2.build_opener()
+	f = opener.open(req)
+	jsonObject = json.load(f)
+	video = jsonObject.get("videos")[number]
+	soup = BeautifulSoup(video.get('embed', None))
+	embedData = soup.find('param', attrs = {'name' : 'flashvars'})['value']
+	return embedData
