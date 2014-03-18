4964cfa308ed82a829cecf4c99726d962720521c
Doug <dougjchau@gmail.com>
2014-03-18 11:28:57 -0400

    add simple video (still test)

--- tweek/views.py
+++ tweek/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
+import urllib2
+import json
+
 # Create your views here.
 def index(request):
-	return render(request, 'tweek/index.html')
+	base = "https://api.twitch.tv/kraken"	
+	req = urllib2.Request(base + "/videos/top")
+	req.add_header('Accept', 'application/vnd.twitchtv.v2+json')
+	opener = urllib2.build_opener()
+	f = opener.open(req)
+	object = json.load(f)
+	thing = object.get("videos")[0]
+	embed = thing.get('embed')
+	context = {
+			'video' : embed
+	}
+	return render(request, 'tweek/index.html', context)
+
+	
